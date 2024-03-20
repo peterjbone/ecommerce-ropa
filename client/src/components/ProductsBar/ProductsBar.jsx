8934b4b277f716ebbffd320d6f73b813afb5af7d
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-03-20 18:29:44 -0300

    Agregar Componentes HeroImage Y ProductBar

--- client/src/components/ProductsBar/ProductsBar.jsx
+++ client/src/components/ProductsBar/ProductsBar.jsx
@@ -1,15 +1,60 @@
-export default function ProductsBar({products}) {
+import './ProductsBar.css'
+
+import { useState } from 'react'
+
+
+export default function ProductsBar({ title, products }) {
+
+  const [productPosition, setProductPosition] = useState(0);
+
+  const [translatePercentage, setTranslatePercentage] = useState(0)
+
+  function showNextImage() {
+    setProductPosition(index => {
+      if (index === products.slice(0, 10).length - 1) return 0
+      return index + 1
+    });
+  }
+
+  function showPrevImage() {
+    setProductPosition(index => {
+      if (index === 0) return products.slice(0, 10).length - 1
+      return index - 1
+    });
+  }
 
   return (
-    <div className='products-bar-container' >
-      {products.map(product => 
-        <div key={product.id} >
-          <img src={product.imagen[0]} alt={product.imagen[0]} />
-          <h4>{product.nombre}</h4>
-          <p>{product.tallas}</p>
-          <p>{product.colores}</p>
+    <div style={{ display:'flex', flexDirection:'column'}} >
+      <h3 className='product-bar-title' >{title}</h3>
+      <div style={{ display: 'flex', width: '80%', margin: 'auto', alignItems: 'center', position: 'relative' }} >
+        <button
+          className='product-arrow-button'
+          disabled={productPosition === 0}
+          onClick={showPrevImage} >
+          ⬅️
+        </button>
+        <div className='products-bar-container' >
+          {products.slice(0, 10).map(product =>
+            <div key={product.id} className='product-container' style={{ translate: `${productPosition * -200}px` }} >
+              <div className='product-image-container' >
+                <img src={product.imagen[0]} alt={product.imagen[0]} className='image' />
+              </div>
+              <h4>{product.nombre}</h4>
+              <p>${product.precio}</p>
+              <button className='product-add-to-cart-button' >Add To Cart</button>
+              {/* <p>{product.tallas}</p>
+          <p>{product.colores}</p> */}
+            </div>
+          )}
         </div>
-        )}
+        <button
+          disabled={productPosition === (products.slice(0, 10).length - 5)}
+          className='product-arrow-button'
+          onClick={showNextImage}
+          style={{ right: 0 }} >
+          ➡️
+        </button>
+      </div>
     </div>
   )
 }
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,9 +1,16 @@
 import CardsContainer from "../../components/CardsContainer/CardsContainer"
+import HeroImagesBar from "../../components/HeroImagesBar/HeroImagesBar";
+import ProductsBar from "../../components/ProductsBar/ProductsBar";
 import products from '../../utils/arrayProductos';
 
 const Home = () => {
   return (
     <div>
+      <HeroImagesBar products={products} />
+      <ProductsBar title='Destacados' products={products} />
+      <ProductsBar title='Nuevos' products={products} />
+      <ProductsBar title='Favoritos' products={products} />
+      <ProductsBar title='Ofertas' products={products} />
       <CardsContainer products={products} />
     </div>
   )
