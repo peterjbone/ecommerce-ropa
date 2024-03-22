da62443d7d2b029462027ce3baab8f0c14115f75
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-03-22 09:10:25 -0300

    Agregar Ruta De Cards En ProductBar Y Arreglo De Errores

--- client/src/components/ProductsBar/ProductsBar.jsx
+++ client/src/components/ProductsBar/ProductsBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './ProductsBar.css'
 
 import { useState } from 'react'
@@ -23,7 +24,7 @@ export default function ProductsBar({ title, products }) {
   }
 
   return (
-    <div style={{ display:'flex', flexDirection:'column'}} >
+    <div style={{ display: 'flex', flexDirection: 'column' }} >
       <h3 className='product-bar-title' >{title}</h3>
       <div style={{ display: 'flex', width: '80%', margin: 'auto', alignItems: 'center', position: 'relative' }} >
         <button
@@ -34,15 +35,15 @@ export default function ProductsBar({ title, products }) {
         </button>
         <div className='products-bar-container' >
           {products?.map(product =>
-            <div key={product.id} className='product-container' style={{ translate: `${productPosition * - 205 * 5 }px` }} >
+            <div key={`${title}/${product.id}`} className='product-container' style={{ translate: `${productPosition * - 205 * 5}px` }} >
               <div className='product-image-container' >
-                <img src={product.imagen[0]} alt={product.imagen[0]} className='image' />
+                <Link to={`/${product.id}`} >
+                  <img src={product.imagen[0]} alt={product.imagen[0]} className='image' />
+                </Link>
               </div>
               <h4>{product.nombre}</h4>
               <p>${product.precio}</p>
               <button className='product-add-to-cart-button' >Agregar Al Carrito</button>
-              {/* <p>{product.tallas}</p>
-          <p>{product.colores}</p> */}
             </div>
           )}
         </div>
