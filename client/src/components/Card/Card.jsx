740cfb71abd148c42fd5c4af6d24a018f4e9ef26
peterjbone <peterjbone41@gmail.com>
2024-03-27 20:41:26 -0500

    Corrigiendo front - Joao

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,39 +1,48 @@
-import './Card.css';
-import { Link } from "react-router-dom"
+import "./Card.css";
+import { Link } from "react-router-dom";
 
 export default function Card({ product }) {
-
-  return (
-    <Link to={`/${product.id}`} key={product.id} >
-      <div className='card-container'>
-        <div className='card-image-container'>
-          <img src={product.imagen[0]} alt={`Imagen producto ${product.name}`} />
-        </div>
-        <div className='card-info-container' >
-          <p style={{ fontSize: '20px'}} >{product.nombre}</p>
-          <p>{product.marca}</p>
-          {product?.tallas && (
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              {product.tallas.map(talla => (
-                <p className='sizes' key={talla}>{talla}</p>
-              ))}
-            </div>
-          )}
-          <div style={{ display: 'flex', flexDirection: 'row' }} >
-            {product.opcion.map((opcion, index) => {
-              const { codHexadecimal } = opcion.color;
-              return (
-                <div
-                  key={index}
-                  className='colorBox'
-                  style={{ backgroundColor: codHexadecimal }}
-                ></div>
-              );
-            })}
-          </div>
-          <p style={{fontSize: '20px'}} >$ {product.precio.toLocaleString('sp-AR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-        </div>
-      </div>
-    </Link>
-  )
+	return (
+		<Link to={`/${product._id}`} key={product._id}>
+			<div className="card-container">
+				<div className="card-image-container">
+					<img
+						src={product.imagen[0]}
+						alt={`Imagen producto ${product.name}`}
+					/>
+				</div>
+				<div className="card-info-container">
+					<p style={{ fontSize: "20px" }}>{product.nombre}</p>
+					<p>{product.marca}</p>
+					{product?.tallas && (
+						<div style={{ display: "flex", flexDirection: "row" }}>
+							{product.tallas.map((talla) => (
+								<p className="sizes" key={talla}>
+									{talla}
+								</p>
+							))}
+						</div>
+					)}
+					<div style={{ display: "flex", flexDirection: "row" }}>
+						{product.opcion.map((opcion, index) => {
+							const { codHexadecimal } = opcion.color;
+							return (
+								<div
+									key={index}
+									className="colorBox"
+									style={{ backgroundColor: codHexadecimal }}></div>
+							);
+						})}
+					</div>
+					<p style={{ fontSize: "20px" }}>
+						${" "}
+						{product.precio.toLocaleString("sp-AR", {
+							minimumFractionDigits: 2,
+							maximumFractionDigits: 2
+						})}
+					</p>
+				</div>
+			</div>
+		</Link>
+	);
 }
--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,11 +1,12 @@
-import Card from "../Card/Card"
-import './CardsContainer.css'
+import Card from "../Card/Card";
+import "./CardsContainer.css";
 
 export default function CardsContainer({ products }) {
-  return (
-    <div className='cards-container'>
-      {products.map(product => (
-      <Card key={product.id} product={product} />))}
-    </div>
-  )
+	return (
+		<div className="cards-container">
+			{products.map((product) => (
+				<Card key={product._id} product={product} />
+			))}
+		</div>
+	);
 }
