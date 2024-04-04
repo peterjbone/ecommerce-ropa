bf35c0e01cd09b1fec42bed0258f022047950b4c
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-04-03 23:25:41 -0300

    Corrección

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import { Link, useParams,useNavigate} from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./Detail.module.css";
 import HeroImagesBarDetail from '../../components/HeroImagesBar/HeroImagesBarDetail';
-import { useStore } from '../../store'; 
+import { useStore } from '../../store';
 
 export default function Detail() {
   const navigate = useNavigate();
@@ -13,7 +13,7 @@ export default function Detail() {
   const [relatedProducts, setRelatedProducts] = useState([]);
   const productoDetail = useStore((state) => state.productoDetail);
   const getProductById = useStore((state) => state.getProductById);
-  const addToCart = useStore((state) => state.addToCart); 
+  const addToCart = useStore((state) => state.addToCart);
   const storedCart = JSON.parse(localStorage.getItem('cart')) || [];
 
   useEffect(() => {
@@ -32,20 +32,20 @@ export default function Detail() {
         if (productoDetail && productoDetail.categoria) {
           const response = await axios.post('http://localhost:3001/productos', {
             categoria: [productoDetail.categoria],
-            busqueda: '' 
+            busqueda: ''
           });
           const filteredProducts = response.data.filteredProducts;
-          const randomProducts = getRandomItems(filteredProducts, 8, id); 
+          const randomProducts = getRandomItems(filteredProducts, 8, id);
           setRelatedProducts(randomProducts);
         }
       } catch (error) {
         console.error("Error al obtener productos relacionados:", error);
       }
     };
-    
+
     fetchRelatedProducts();
   }, [id]);
-  
+
 
   const handleColorChange = (index) => {
     setSelectedColorIndex(index);
@@ -58,12 +58,12 @@ export default function Detail() {
 
   const handleAddToCart = () => {
     if (selectedSizeIndex !== null && selectedColorIndex !== null) {
-      const selectedColor = productoDetail.opciones[selectedColorIndex].colores.nombres[0];     
+      const selectedColor = productoDetail.opciones[selectedColorIndex].colores.nombres[0];
       const selectedSize = productoDetail.opciones[selectedColorIndex].tallas[selectedSizeIndex];
 
       if (selectedSize.stock > 0) {
         const variantId = `${productoDetail._id}-${selectedColor}-${selectedSize.talla}`;
-  
+
         const selectedProduct = {
           id: productoDetail._id,
           nombre: productoDetail.nombre,
@@ -82,18 +82,18 @@ export default function Detail() {
           quantity: 1,
           variantId: variantId
         };
-  
+
         addToCart(selectedProduct);
-        
+
         const storedCart = JSON.parse(localStorage.getItem('cart')) || [];
         const existingProduct = storedCart.find(item => item.variantId === variantId);
 
         if (existingProduct) {
-        existingProduct.quantity += 1;
+          existingProduct.quantity += 1;
         } else {
-        storedCart.push({ ...selectedProduct, variantId });
+          storedCart.push({ ...selectedProduct, variantId });
         }
-       localStorage.setItem('cart', JSON.stringify(storedCart));
+        localStorage.setItem('cart', JSON.stringify(storedCart));
 
       } else {
         console.log('Esta talla está agotada');
@@ -102,14 +102,14 @@ export default function Detail() {
       console.log('Por favor selecciona color y talla');
     }
   };
-  
+
   const getRandomItems = (array, count, currentProductId) => {
     const shuffled = array
       .filter(product => product._id !== currentProductId)
       .sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
   };
-  
+
 
   const editHandler = () => {
     navigate(`/editproduct/${id}`);
@@ -123,83 +123,84 @@ export default function Detail() {
         </p>
       </div>
     );
-  }else{ 
-  return (
-    <div className={styles.detailContainer}>
-      {productoDetail && (
-        <div className={styles.imagen}>
-          <HeroImagesBarDetail product={productoDetail} selectedColorIndex={selectedColorIndex} />
-        </div>
-      )}
-
-      <div className={styles.containerDatos}>
-        <h2 className={styles.productName}>{productoDetail && productoDetail.nombre}</h2>
-        <p><strong>Descripción:</strong> {productoDetail && productoDetail.descripcion}</p>
-        <p><strong>Marca:</strong> {productoDetail && productoDetail.marca}</p>
-        <p><strong>Género:</strong> {productoDetail && productoDetail.genero}</p>
-        <p><strong>Categoria:</strong>{productoDetail && productoDetail.categoria}</p>
-        <p><strong>SubCategoria:</strong> {productoDetail && productoDetail.subcategoria}</p>
-        <p><strong>Precio:</strong> ${productoDetail && productoDetail.precio}</p>
-        {productoDetail && productoDetail.oferta && (
-          <p><strong>Oferta: </strong> {productoDetail.oferta.Descuento}% de descuento</p>
+  } else {
+    return (
+      <div className={styles.detailContainer}>
+        {productoDetail && (
+          <div className={styles.imagen}>
+            <HeroImagesBarDetail product={productoDetail} selectedColorIndex={selectedColorIndex} />
+          </div>
         )}
 
-        <div>
-          <h3>Opciones:</h3>
-          {productoDetail && productoDetail.opciones && productoDetail.opciones.map((opcion, index) => (
-            <div key={index}>
-              <p>Color:</p>
-              <div className={styles.colorsList}>
-                {opcion.colores.codigosHex.map((color, colorIndex) => (
-                  <div
-                    key={colorIndex}
-                    className={`${styles.colorCircle} ${selectedColorIndex === index ? styles.selectedColor : ''}`}
-                    style={{ backgroundColor: color }}
-                    onClick={() => handleColorChange(index)}
-                  ></div>
+        <div className={styles.containerDatos}>
+          <h2 className={styles.productName}>{productoDetail && productoDetail.nombre}</h2>
+          <p><strong>Descripción:</strong> {productoDetail && productoDetail.descripcion}</p>
+          <p><strong>Marca:</strong> {productoDetail && productoDetail.marca}</p>
+          <p><strong>Género:</strong> {productoDetail && productoDetail.genero}</p>
+          <p><strong>Categoria:</strong>{productoDetail && productoDetail.categoria}</p>
+          <p><strong>SubCategoria:</strong> {productoDetail && productoDetail.subcategoria}</p>
+          <p><strong>Precio:</strong> ${productoDetail && productoDetail.precio}</p>
+          {productoDetail && productoDetail.oferta && (
+            <p><strong>Oferta: </strong> {productoDetail.oferta.Descuento}% de descuento</p>
+          )}
 
-                ))}
-              </div>
-              {selectedColorIndex === index && (
-                <div className={styles.sizeOptions}>
-                  <p>Tallas disponibles:</p>
-                  <div className={styles.sizeList}>
-                    {opcion.tallas.map((tallaData, sizeIndex) => (
-                      <div
-                        key={sizeIndex}
-                        className={`${styles.sizeItem} ${selectedSizeIndex === sizeIndex ? styles.selectedSize : ''}`}
-                        onClick={() => handleSizeChange(sizeIndex)}
-                      >
-                        {tallaData.talla} (Stock: {tallaData.stock})
-                      </div>
-                    ))}
+          <div>
+            <h3>Opciones:</h3>
+            {productoDetail && productoDetail.opciones && productoDetail.opciones.map((opcion, index) => (
+              <div key={index}>
+                <p>Color:</p>
+                <div className={styles.colorsList}>
+                  {opcion.colores.codigosHex.map((color, colorIndex) => (
+                    <div
+                      key={colorIndex}
+                      className={`${styles.colorCircle} ${selectedColorIndex === index ? styles.selectedColor : ''}`}
+                      style={{ backgroundColor: color }}
+                      onClick={() => handleColorChange(index)}
+                    ></div>
+
+                  ))}
+                </div>
+                {selectedColorIndex === index && (
+                  <div className={styles.sizeOptions}>
+                    <p>Tallas disponibles:</p>
+                    <div className={styles.sizeList}>
+                      {opcion.tallas.map((tallaData, sizeIndex) => (
+                        <div
+                          key={sizeIndex}
+                          className={`${styles.sizeItem} ${selectedSizeIndex === sizeIndex ? styles.selectedSize : ''}`}
+                          onClick={() => handleSizeChange(sizeIndex)}
+                        >
+                          {tallaData.talla} (Stock: {tallaData.stock})
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-        <div className={styles.buttonsContainer}>
-          <button className={styles.addToCartButton} onClick={handleAddToCart}>Agregar al carrito</button>
-          {storedCart.length === 0 && <button className={styles.buyButton}>Comprar</button>}
-          <button className={styles.buyButton} onClick={editHandler}>
-                  Editar producto
-                </button>
-        </div>
-      </div>
-      <h2 className={styles.productsRelacionados}>Productos relacionados</h2>
-      <div className={styles.relatedProductsContainer}>
-        <div className={styles.relatedProductsWrapper}>
-          <div className={styles.relatedProducts} >
-            {relatedProducts.map((relatedProduct) => (
-              <Link to={`/${relatedProduct._id}`} key={relatedProduct._id} className={styles.relatedProductLink}>
-                <div className={styles.relatedProduct}>
-                  <img src={relatedProduct.opciones[0]?.imagenes[0]} alt={relatedProduct.nombre} className={styles.relatedProductImageLarge} />
-                  <h3 className={styles.relatedProductName}>{relatedProduct.nombre}</h3>
-                  <p className={styles.relatedProductPrice}>${relatedProduct.precio}</p>
-                </div>
-              </Link>
+                )}
+              </div>
             ))}
+          </div>
+          <div className={styles.buttonsContainer}>
+            <button className={styles.addToCartButton} onClick={handleAddToCart}>Agregar al carrito</button>
+            {storedCart.length === 0 && <button className={styles.buyButton}>Comprar</button>}
+            <button className={styles.buyButton} onClick={editHandler}>
+              Editar producto
+            </button>
+          </div>
+        </div>
+        <h2 className={styles.productsRelacionados}>Productos relacionados</h2>
+        <div className={styles.relatedProductsContainer}>
+          <div className={styles.relatedProductsWrapper}>
+            <div className={styles.relatedProducts} >
+              {relatedProducts.map((relatedProduct) => (
+                <Link to={`/${relatedProduct._id}`} key={relatedProduct._id} className={styles.relatedProductLink}>
+                  <div className={styles.relatedProduct}>
+                    <img src={relatedProduct.opciones[0]?.imagenes[0]} alt={relatedProduct.nombre} className={styles.relatedProductImageLarge} />
+                    <h3 className={styles.relatedProductName}>{relatedProduct.nombre}</h3>
+                    <p className={styles.relatedProductPrice}>${relatedProduct.precio}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
