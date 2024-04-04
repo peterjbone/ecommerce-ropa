8a1b17cf509ead74ceaa87fd10a3b7b0a063866c
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-04-04 16:16:03 -0300

    Actualización Para Deploy

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const server = require("./server.js");
 const PORT = 3001;
 const { connectDB, insertarDocumentos } = require("./db.js");
 
-server.listen(PORT, async () => {
-  console.log(`Servidor levantado en el puerto: ${PORT} (backend)`);
+const port = process.env.PORT || 3000;
+
+server.listen(port, "0.0.0.0", async () => {
   await connectDB();
   // await insertarDocumentos();
-});
+  console.log(`Server listening on port ${port}`);
+})
