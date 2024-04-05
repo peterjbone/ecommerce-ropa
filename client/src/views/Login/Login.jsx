14cd0c995f65916edd6b76b545e9fc1f1492a3b8
Victor <vicktorcori@hotmail.com>
2024-04-05 16:38:31 -0300

    login y register primer contacto con endpoints del back

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,8 +1,13 @@
 import styles from './Login.module.css'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const Login = () => {
+
+  const navigate = useNavigate()
 
   const [user, setUser] = useState({
     email: '',
@@ -14,9 +19,35 @@ const Login = () => {
     setUser({...user, [name]: value})
   }
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault()
-    console.log(user);
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', user)
+      console.log(response.status);
+      if(response.status === 200) {
+        toast.success('Iniciaste sesión', {
+          onClose: () => {
+            navigate('/');
+          },
+          autoClose: 1000,
+        });
+
+        setUser({ // Clear the form
+          fullname: '',
+          name: '',
+          surname: '',
+          email: '',
+          password: '',
+          address: ''
+        });
+
+      } else {
+        // Handle other response statuses
+        toast.error('Error al iniciar sesión');
+      }
+    } catch (error) {
+      toast.error('Error al iniciar sesión');
+    }
   }
   return (
     <div className={styles.container}>
@@ -29,6 +60,7 @@ const Login = () => {
         <span className={styles.span}>¿Olvidaste tu contraseña?</span>
         <button className={styles.button} type='submit'>Login</button>
         <Link to='/register'><span className={styles.span}>¿No tienes cuenta? Registrate</span></Link>
+        <ToastContainer />
       </form>
     </div>
   )
