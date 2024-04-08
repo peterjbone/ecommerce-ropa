6aea27b08bc006ed1870fca9023b10bb13a460a0
Gaston <gastonmonzon3@gmail.com>
2024-04-08 04:29:29 -0300

    Agregar Funciones De Usuario Firebase

--- server/src/firebase/firebaseConfig.js
+++ server/src/firebase/firebaseConfig.js
@@ -1,14 +1,32 @@
-import { initializeApp } from 'firebase/app';
+const { initializeApp } = require('firebase/app');
+const { getAuth, setPersistence } = require('firebase/auth');
+const dotenv = require('dotenv');
 
-// TODO: Replace the following with your app's Firebase project configuration
+dotenv.config();
+const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } = process.env;
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  //...
-  apiKey: "AIzaSyDePYMDQwicejlpP9o6SvF3EznkmIdYNyE",
-  authDomain: "ecommerce-ropa-fc64e.firebaseapp.com",
-  projectId: "ecommerce-ropa-fc64e",
-  storageBucket: "ecommerce-ropa-fc64e.appspot.com",
-  messagingSenderId: "34740119574",
-  appId: "1:34740119574:web:b48f3c1c970d84f959ed37"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+setPersistence(auth, 'local') // No me acuerdo para que servía esto exactamente pero tengo miedo de borrarlo
+  .then(() => {
+    // Continue with other Firebase operations
+  })
+  .catch((error) => {
+    // Handle persistence enablement error
+    console.error('Error enabling persistence:', error);
+  });
+
+module.exports = auth;
