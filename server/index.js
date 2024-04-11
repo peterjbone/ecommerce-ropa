55915ee0647b91a8620fa96eb585a7a5086d94af
Gaston <gastonmonzon3@gmail.com>
2024-04-11 17:10:49 -0300

    Corrección 4

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 server.listen(port, "0.0.0.0", async () => {
 	console.log(`Servidor levantado en el puerto: ${port} (backend)`);
 	await connectDB(); // Descomentar lo q sea necesario
-	await insertarTodo();
+	// await insertarTodo();
 	// await insertarProductos();
 	// await insertarResenas();
 });
